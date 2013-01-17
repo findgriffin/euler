33546d05c04640ead36e877abe37e577e8c931c3
David Griffin <findgriffin@gmail.com>
2013-01-17 14:31:25 +1100

    make prob001.py executable and add docstring

--- prob001/prob001.py
+++ prob001/prob001.py
@@ -1,11 +1,12 @@
-# problem 1
+#! /usr/bin/python
+""" prob001.py """
 # add all the multiples of 3 and 5 below 1000
 # 
 
-total = 0
+TOTAL = 0
 
 for i in xrange(1000):
     if i % 3 == 0 or i % 5 == 0:
-        total += i
-print total
+        TOTAL += i
+print TOTAL
 
